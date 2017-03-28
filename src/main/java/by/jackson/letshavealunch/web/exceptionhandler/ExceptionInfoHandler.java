dab7395ae1264f4aa94b587a0cfebcced75216f6
jacksn <gml.jackson@gmail.com>
2017-03-28 22:29:40 +0300

    Cleanup

--- src/main/java/by/jackson/letshavealunch/web/exceptionhandler/ExceptionInfoHandler.java
+++ src/main/java/by/jackson/letshavealunch/web/exceptionhandler/ExceptionInfoHandler.java
@@ -1,45 +1,42 @@
 package by.jackson.letshavealunch.web.exceptionhandler;
 
-import by.jackson.letshavealunch.util.ValidationUtil;
 import by.jackson.letshavealunch.util.exception.ErrorInfo;
 import by.jackson.letshavealunch.util.exception.NotFoundException;
 import by.jackson.letshavealunch.util.exception.VotingEndedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
 @ControllerAdvice(annotations = RestController.class)
 public class ExceptionInfoHandler {
     private static final Logger LOG = LoggerFactory.getLogger(ExceptionInfoHandler.class);
 
-    private static Map<String, String> constraintCodeMap = new HashMap<String, String>() {
-        {
-            put("users_unique_email_idx", "exception.users.duplicate_email");
-            put("restaurants_unique_name_idx", "exception.common.duplicate_name");
-        }
-    };
+    private static ErrorInfo logAndGetValidationErrorInfo(HttpServletRequest req, BindingResult result) {
+        String[] details = result.getFieldErrors().stream()
+                .map(fe -> fe.getField() + ' ' + fe.getDefaultMessage())
+                .toArray(String[]::new);
 
-    @Autowired
-    private MessageSource messageSource;
+        LOG.warn("Validation exception at request " + req.getRequestURL() + ": " + Arrays.toString(details));
+        return new ErrorInfo(req.getRequestURL(), "ValidationException", details);
+    }
+
+    private static ErrorInfo logAndGetErrorInfo(HttpServletRequest req, Exception e, boolean logException) {
+        if (logException) {
+            LOG.error("Exception at request " + req.getRequestURL(), e);
+        } else {
+            LOG.warn("Exception at request " + req.getRequestURL() + ": " + e.toString());
+        }
+        return new ErrorInfo(req.getRequestURL(), e);
+    }
 
     //  http://stackoverflow.com/a/22358422/548473
     @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY) // 422
@@ -60,18 +57,6 @@ public class ExceptionInfoHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     @ResponseBody
     public ErrorInfo conflict(HttpServletRequest req, DataIntegrityViolationException e) {
-        String rootMsg = ValidationUtil.getRootCause(e).getMessage();
-        if (rootMsg != null) {
-            Optional<Map.Entry<String, String>> entry = constraintCodeMap.entrySet().stream()
-                    .filter((it) -> rootMsg.contains(it.getKey()))
-                    .findAny();
-            if (entry.isPresent()) {
-                return logAndGetErrorInfo(req,
-                        new DataIntegrityViolationException(
-                                messageSource.getMessage(entry.get().getValue(), null, LocaleContextHolder.getLocale())
-                        ), false);
-            }
-        }
         return logAndGetErrorInfo(req, e, true);
     }
 
@@ -101,23 +86,5 @@ public class ExceptionInfoHandler {
     @ResponseBody
     public ErrorInfo handleError(HttpServletRequest req, Exception e) {
         return logAndGetErrorInfo(req, e, true);
-    }
-
-    public static ErrorInfo logAndGetValidationErrorInfo(HttpServletRequest req, BindingResult result) {
-        String[] details = result.getFieldErrors().stream()
-                .map(fe -> fe.getField() + ' ' + fe.getDefaultMessage())
-                .toArray(String[]::new);
-
-        LOG.warn("Validation exception at request " + req.getRequestURL() + ": " + Arrays.toString(details));
-        return new ErrorInfo(req.getRequestURL(), "ValidationException", details);
-    }
-
-    public static ErrorInfo logAndGetErrorInfo(HttpServletRequest req, Exception e, boolean logException) {
-        if (logException) {
-            LOG.error("Exception at request " + req.getRequestURL(), e);
-        } else {
-            LOG.warn("Exception at request " + req.getRequestURL() + ": " + e.toString());
-        }
-        return new ErrorInfo(req.getRequestURL(), e);
     }
 }
