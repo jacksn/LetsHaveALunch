16f68993484e94573bb66321540cc9b3aeb38c3a
jacksn <gml.jackson@gmail.com>
2017-04-05 14:38:37 +0300

    Cleanup

--- src/test/java/by/jackson/letshavealunch/service/AbstractServiceTest.java
+++ src/test/java/by/jackson/letshavealunch/service/AbstractServiceTest.java
@@ -14,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
