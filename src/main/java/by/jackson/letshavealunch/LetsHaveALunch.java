ebb4472d11e280ebd1f4b534e32d9602f5618ec5
jacksn <gml.jackson@gmail.com>
2017-04-05 13:51:30 +0300

    Refactoring

--- src/main/java/by/jackson/letshavealunch/LetsHaveALunch.java
+++ src/main/java/by/jackson/letshavealunch/LetsHaveALunch.java
@@ -2,9 +2,11 @@ package by.jackson.letshavealunch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SuppressWarnings("CheckStyle")
 @SpringBootApplication
+@EnableTransactionManagement
 public class LetsHaveALunch {
 
     public static void main(String[] args) {
