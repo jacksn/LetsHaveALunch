7ffc1fa52bca8099cb8358e2ba3318eb8b9a7f39
jacksn <gml.jackson@gmail.com>
2017-04-05 12:07:09 +0300

    Add restaurants to Spring cache

--- src/main/java/by/jackson/letshavealunch/service/RestaurantServiceImpl.java
+++ src/main/java/by/jackson/letshavealunch/service/RestaurantServiceImpl.java
@@ -4,6 +4,8 @@ import by.jackson.letshavealunch.model.Restaurant;
 import by.jackson.letshavealunch.repository.RestaurantRepository;
 import by.jackson.letshavealunch.util.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
@@ -19,14 +21,14 @@ public class RestaurantServiceImpl implements RestaurantService {
     @Autowired
     private RestaurantRepository repository;
 
-//    @CacheEvict(value = "restaurants", allEntries = true)
+    @CacheEvict(value = "restaurants", allEntries = true)
     @Override
     public Restaurant save(Restaurant restaurant) {
         Assert.notNull(restaurant, "restaurant must not be null");
         return repository.save(restaurant);
     }
 
-//    @CacheEvict(value = "restaurants", allEntries = true)
+    @CacheEvict(value = "restaurants", allEntries = true)
     @Override
     public void delete(int id) {
         checkNotFoundWithId(repository.delete(id) != 0, id);
@@ -37,20 +39,20 @@ public class RestaurantServiceImpl implements RestaurantService {
         return checkNotFoundWithId(repository.findOne(id), id);
     }
 
-//    @Cacheable("restaurants")
+    @Cacheable("restaurants")
     @Override
     public List<Restaurant> getAll() {
         return repository.findAll(SORT_NAME);
     }
 
-//    @CacheEvict(value = "restaurants", allEntries = true)
+    @CacheEvict(value = "restaurants", allEntries = true)
     @Override
     public void update(Restaurant restaurant) {
         Assert.notNull(restaurant, "restaurant must not be null");
         repository.save(restaurant);
     }
 
-//    @CacheEvict(value = "restaurants", allEntries = true)
+    @CacheEvict(value = "restaurants", allEntries = true)
     @Override
     public void evictCache() {
     }
